a5307a3142b8d502887b9d264c0ee8e4db73ccec
Nick Smith <nick.smith@cern.ch>
2020-10-07 12:31:34 -0500

    Better coro

--- dmwmclient/asyncutil.py
+++ dmwmclient/asyncutil.py
@@ -1,7 +1,27 @@
 import asyncio
 
 
-async def completed(futures):
-    """as_completed as an async generator"""
-    for future in asyncio.as_completed(list(futures)):
-        yield await future
+async def completed(coroutines, semaphore=None):
+    """as_completed as an async generator
+
+    Takes a list of coroutines, and executes them as tasks, yielding
+    the results as they are completed.
+
+    Parameters
+    ----------
+        semaphore : AbstractAsyncContextManager or int, optional
+            A semaphore to limit concurrency
+    """
+    if semaphore is None:
+        for coro in asyncio.as_completed(list(coroutines)):
+            yield await coro
+    else:
+        if isinstance(semaphore, int):
+            semaphore = asyncio.BoundedSemaphore(semaphore)
+
+        async def throttle(coro):
+            async with semaphore:
+                return await coro
+
+        for coro in asyncio.as_completed(map(throttle, coroutines)):
+            yield await coro
